73914153e69ea03fb72133dbcfd86f76ae300018
nujabes <nujabes403@gmail.com>
2015-03-03 22:57:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Hello It's nujabes403's flying.
+Go for it!
---
+++ censortext.js
@@ -0,0 +1,27 @@
+var censoredWords = ["sad","bad","mad"];
+
+var customCensoredWords = [];
+
+function censor(inStr){
+    for(idx in censoredWords){
+        inStr = inStr.replace(censoredWords[idx],"****");
+    }
+    for(idx in customCensoredWords){
+        inStr = inStr.replace(customCensoredWords[idx],"****");
+    }
+    return inStr;
+}
+
+function addCensoredWords(word) {
+    return censoredWords.push(word);
+}
+
+function getCensoredWords(){
+    return censoredWords.concat(customCensoredWords);
+}
+
+exports.censor = censor;
+exports.addCensoredWords = addCensoredWords;
+exports.getCensoredWords = getCensoredWords;
+
+
